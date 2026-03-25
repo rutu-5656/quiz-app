4ef200f8c02d867e78c7a24911dcb30e4df3187d
rutu-5656 <ruturajx@gmail.com>
2026-03-25 18:52:06 +0530

    Fix CORS issue

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 # CORS for React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
